0f57f2adac0a460b0d261bf59ad659606391300d
Phuoc <phuocnguyen.081096@gmail.com>
2022-03-09 08:51:02 +0700

    gitproject'

--- New folder (2)/Sudungtoantu/toantu.js
+++
@@ -1,8 +0,0 @@
-let inputWidth;
-let inputHeight;
-inputWidth = prompt("Enter the width");
-inputHeight = prompt("Enter the height");
-let Width = parseInt(inputWidth);
-let Height = parseInt(inputHeight);
-let area = Width * Height;
-document.write("The area is : " + area);
